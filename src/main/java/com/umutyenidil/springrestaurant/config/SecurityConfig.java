4a5ff3068989f9ac4443924d8b8650e851e45a34
Umut Yenidil <umut.yenidil9@gmail.com>
2025-04-25 16:31:51 +0300

    resimlerin getirilmesi saglandi

--- src/main/java/com/umutyenidil/springrestaurant/config/SecurityConfig.java
+++ src/main/java/com/umutyenidil/springrestaurant/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.umutyenidil.springrestaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +17,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(
-                        auth -> auth.anyRequest().authenticated()
+                        auth -> auth
+                                .requestMatchers(HttpMethod.GET, "/api/v1/photos/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(
                         oauth2 -> oauth2.jwt(
@@ -24,7 +27,8 @@ public class SecurityConfig {
                         )
                 )
                 .sessionManagement(
-                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .csrf(
                         csrf -> csrf.disable()
--- src/main/java/com/umutyenidil/springrestaurant/controllers/PhotoController.java
+++ src/main/java/com/umutyenidil/springrestaurant/controllers/PhotoController.java
@@ -5,10 +5,12 @@ import com.umutyenidil.springrestaurant.domain.entities.Photo;
 import com.umutyenidil.springrestaurant.mappers.PhotoMapper;
 import com.umutyenidil.springrestaurant.services.PhotoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -25,5 +27,21 @@ public class PhotoController {
         Photo savedPhoto = photoService.uploadPhoto(file);
 
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(
+            @PathVariable("id") String id
+    ) {
+        return photoService.getPhotoAsResource(id)
+                .map(photo -> ResponseEntity.ok()
+                        .contentType(
+                                MediaTypeFactory.getMediaType(photo)
+                                        .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                        )
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                        .body(photo)
+                )
+                .orElse(ResponseEntity.notFound().build());
     }
 }
